800b5d0edda9cce3495942ef26c11c8503a81262
Rafael <mrrafaeldeoliveira@gmail.com>
2023-12-15 16:31:10 -0300

    feat: ajust home page

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@ import BottomApp from './components/BottomApp.vue';
 import router from './router';
 import { NConfigProvider, NMessageProvider, NDialogProvider } from 'naive-ui'
 import { ptBR, datePtBR } from 'naive-ui'
-import { ref } from 'vue';
+import InstallPage from './views/InstallPage.vue'
+import { ref, onBeforeMount, computed } from 'vue';
 
 const isLogged = () => router.currentRoute.value.name !== 'login';
 
@@ -15,14 +16,48 @@ function updateMonth(m: number) {
     month.value = m;
 }
 
+const displayMode = ref(getPWADisplayMode());
+
+const showApp = computed(() => displayMode.value !== 'browser');
+const installEvent = ref<any>();
+
+function getPWADisplayMode() {
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+    if (document.referrer.startsWith('android-app://')) {
+        return 'twa';
+    } else if (isStandalone) {
+        return 'standalone';
+    }
+    return 'browser';
+}
+
+onBeforeMount(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault()
+        installEvent.value = e
+    })
+
+    window.matchMedia('(display-mode: standalone)').addEventListener('change', (evt) => {
+        let dm = 'browser';
+        if (evt.matches) {
+            dm = 'standalone';
+        }
+        displayMode.value = dm;
+    });
+})
+
+function installPWA() {
+    installEvent.value.prompt();
+}
 </script>
 
 <template>
     <n-config-provider :locale="ptBR" :date-locale="datePtBR">
         <n-message-provider>
             <n-dialog-provider>
-                <div>
-                    <top-app v-if="isLogged()" @month-change="updateMonth($event!)"/>
+                <install-page v-if="!showApp" @install="installPWA"></install-page>
+                <div v-if="showApp">
+                    <top-app v-if="isLogged()" @month-change="updateMonth($event!)" />
                     <RouterView />
                     <bottom-app v-if="isLogged()" />
                 </div>
